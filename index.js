890eda1e4b89ce0d78ff207f34486c326728a89e
JRamsoobhag06 <jacob.ramsoobhag@gmail.com>
2026-03-04 23:07:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+function getAverageGrade(student, course) {
+  for (let entry of student.transcript) {
+    if (entry.course === course) {
+      let sum = 0;
+      for (let grade of entry.grades) {
+        sum += grade;
+      }
+      return sum / entry.grades.length;
+    }
+  }
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+  return -1;
+}
+
+function getAssignmentMark(student, course, num) {
+  for (let entry of student.transcript) {
+    if (entry.course === course) {
+      if (num >= 0 && num < entry.grades.length) {
+        return entry.grades[num];
+      }
+    }
+  }
+
+  return -1;
+}
+
+function averageAssessment(students, courseName, assignment) {
+  let sum = 0;
+  let count = 0;
+
+  for (let student of students) {
+    for (let entry of student.transcript) {
+      if (entry.course === courseName) {
+        if (assignment >= 0 && assignment < entry.grades.length) {
+          sum += entry.grades[assignment];
+          count++;
+        }
+      }
+    }
+  }
+
+  return count > 0 ? sum / count : -1;
 }
